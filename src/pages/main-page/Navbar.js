c7157cda2d27bcb8f15cd452334c8bdbd7943b3a
Tanay <tanayghoriwala001@gmail.com>
2024-01-09 13:45:40 +0530

    some css changes

--- src/pages/main-page/Navbar.js
+++ src/pages/main-page/Navbar.js
@@ -1,11 +1,11 @@
 import NavBottom from "../../component/NavBottom"
 import Navtop from "../../component/Navtop"
 import "./Navbar.css"
-
 const Navbar = () => {
+
   return (
     <>
-    <div className="navbar-main">
+    <div className="navbar-main" >
         <div className="navbar-inner">
           <Navtop/>
           <NavBottom/>
